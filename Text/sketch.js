fe89ff713968b73df58a0e0a14924ba650f008dd
sebyakin <a.sebyakin@gmail.com>
2019-01-08 19:12:05 +0300

    type only

--- Text/sketch.js
+++ Text/sketch.js
@@ -1,19 +1,17 @@
 var textfield;
-var output;
 
 function setup() {
   noCanvas();
-  textfield = createInput('Your name?');
-  textfield.changed(newText);
+  textfield = createInput();
+  // textfield.changed(newText);
   textfield.input(newTyping);
-  output = select('#output');	
+
 }
 
 function newTyping() {
-	output.html(textfield.value());
-  //createP(textfield.value());
-}
-
-function newText() {
   createP(textfield.value());
 }
+
+// function newText() {
+//   createP(textfield.value());
+// }
