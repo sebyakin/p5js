2836c1e3551008a1470b71020e14b10666e0a5d0
sebyakin <a.sebyakin@gmail.com>
2019-01-08 18:41:41 +0300

    hello

--- Text/sketch.js
+++ Text/sketch.js
@@ -1,17 +1,19 @@
 var textfield;
+var output;
 
 function setup() {
   noCanvas();
-  textfield = createInput();
-  // textfield.changed(newText);
+  textfield = createInput('Your name?');
+  textfield.changed(newText);
   textfield.input(newTyping);
-
+  output = select('#output');	
 }
 
 function newTyping() {
-  createP(textfield.value());
+	output.html(textfield.value());
+  //createP(textfield.value());
 }
 
-// function newText() {
-//   createP(textfield.value());
-// }
+function newText() {
+  createP(textfield.value());
+}
